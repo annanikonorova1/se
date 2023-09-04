c75801fd999627667050f6fd65ebfb12d81318f0
annanikonorova1 <131855552+annanikonorova1@users.noreply.github.com>
2023-09-04 18:31:40 +0300

    Add files via upload

---
+++ f.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    context = {'title':'Main'}
+    return render_template('index9.html', **context)
+
+
+@app.route('/clothes/')
+def clothes():
+    context ={'title': 'Clothes'}
+    return render_template('clothes.html', **context)
+
+
+@app.route('/shoes/')
+def shoes():
+    context = {'title': 'Shoes'}
+    return render_template('shoes.html', **context)
+
+
+@app.route('/jacket/')
+def jacket():
+    context = {'title': 'Jacket'}
+    return render_template('jacket.html', **context)
+
+
+if __name__ == '__main__':
+    app.run()
